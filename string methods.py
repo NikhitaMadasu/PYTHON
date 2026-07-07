0f5b45ed7eaae643f54979f06f1c1df95ff070b7
NikhitaMadasu <sainikhitamadasu@gmail.com>
2026-07-07 06:38:52 +0530

    Add files via upload

---
+++ string methods.py
@@ -0,0 +1,16 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #string methods
+>>> #len
+>>> a="python"
+>>> len(a)
+6
+>>> b="python course"
+>>> len(b)
+13
+>>> c=""
+>>> len(c)
+0
+>>> d=" "
+>>> len(d)
+1
