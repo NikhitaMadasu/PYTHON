aac6c11e6641132bd3f179a271bcf8b8b3d29541
NikhitaMadasu <sainikhitamadasu@gmail.com>
2026-07-07 06:41:50 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,409 @@
+#condtions
+#if- condition by using comparsion  operator
+#<,>,<=,>=,!=,==
+
+'''a=10
+b=20
+if a<b:
+    print("true")'''
+
+
+'''a=10
+b=20
+if a>b:
+    print("true")'''
+
+
+'''a=5
+b=7
+if a<=b:
+    print("less")'''
+
+
+'''a=12
+b=15
+if a>=b:
+    print("true")'''
+
+
+'''a=30
+b=40
+if a!=b:
+    print("true")'''
+
+
+'''a=10
+b=10
+if a==b:
+    print("true")'''
+
+
+'''a="python"
+if a=="python":
+    print("match")'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+'''a=int(input("a value"))
+if a<50:
+    print("less")'''
+
+#if-condition by using logical operator
+#and,or,not
+'''a=3
+b=6
+if a<b and b>a:
+    print("true")'''
+
+
+'''a=4
+b=7
+if a<=b and b>=a:
+    print("true")'''
+
+'''a=9
+b=12
+if a!=b and b==a:
+    print("true")'''
+
+'''a=2
+b=4
+if a<b or b>a:
+    print("true")'''
+
+'''a=14
+b=16
+if a<=b or b>=a:
+    print("true")'''
+
+'''a=3
+b=6
+if a!=b and b==a:
+    print("true")'''
+
+'''a=5
+b=7
+if not a>b:
+    print("true")'''
+
+'''a=3
+b=6
+if not a>b:
+    print("true")'''
+
+'''a=3
+b=6
+if not a<b and b>a:
+    print("true")'''
+
+'''a=3
+b=6
+if not a<b or b>a:
+    print("true")'''
+
+#if-condition by using identify operators
+#is,is not
+'''a=4
+if type(a) is int:
+    print("it is int")'''
+
+'''a=4
+if type(a) is not int:
+    print("it is int")'''
+
+
+'''a=4.5
+if type(a) is float:
+    print("it is float")'''
+
+'''a=4.5
+if type(a) is not float:
+    print("it is float")'''
+
+'''a="nikhita"
+if type(a) is str:
+    print("it is str")'''
+
+
+'''a="nikhita"
+if type(a) is not str:
+    print("it is str")'''
+
+
+'''a=4+5j
+if type(a) is complex:
+    print("it is complex")'''
+
+'''a=4+5j
+if type(a) is not complex:
+    print("it is complex")'''
+
+
+'''a=True
+if type(a) is bool:
+    print("it is bool")'''
+
+
+'''a=True
+if type(a) is not bool:
+    print("it is bool")'''
+
+#if-conditions by using membership operators
+#in,not in
+'''a=2,3,4,5,6,7,8,9,10
+if 8 in a:
+    print("true")'''
+
+
+'''a=2,3,4,5,6,7,8,9,10
+if 20 not in a:
+    print("true")'''
+
+'''a=int(input("a value"))
+if 30 in a:
+    print("true")#error'''
+
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input("value"))
+if b in a:
+print("true")'''
+
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input("value"))
+if b not in a:
+    print("true")'''
+            
+#if-else condition by using comparsion operator
+#<,>,<=,>=,!=,==
+'''a=4
+b=8
+if a<b:
+    print("less")
+else:
+    print("false")'''
+
+
+'''a=4
+b=8
+if a>b:
+    print("less")
+else:
+    print("false")'''
+
+
+'''a=4
+b=8
+if a<=b:
+    print("less")
+else:
+    print("false")'''
+
+
+'''a=4
+b=8
+if a>=b:
+    print("less")
+else:
+    print("false")'''
+
+
+'''a=4
+b=8
+if a!=b:
+    print("less")
+else:
+    print("false")'''
+    
+
+'''a=4
+b=8
+if a==b:
+    print("less")
+else:
+    print("false")'''
+#if-else condition by using logical operator
+#and,or,not
+
+'''a=4
+b=7
+if a<=b and b>=a:
+    print("true")
+else:
+    print("false")'''
+
+
+'''a=3
+b=6
+if a<b and b>a:
+    print("true")
+else:
+    print("false")'''
+
+
+'''a=9
+b=12
+if a!=b and b==a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=2
+b=4
+if a<b or b>a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=14
+b=16
+if a<=b or b>=a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=3
+b=6
+if a!=b and b==a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=5
+b=7
+if not a>b:
+    print("true")
+else:
+    print("false")'''
+
+'''a=3
+b=6
+if not a>b:
+    print("true")
+else:
+    print("false")'''
+
+'''a=3
+b=6
+if not a<b and b>a:
+    print("true")
+else:
+    print("false")'''
+
+#if-else condition by using identify operators
+#is,is not
+'''a=4
+if type(a) is int:
+    print("it is int")
+else:
+    print("it is not int")'''
+
+
+'''a=4
+if type(a) is not int:
+    print("it is int")
+else:
+    print("it is not int")'''
+
+
+'''a=4.5
+if type(a) is float:
+    print("it is float")
+else:
+    print("it is not float")'''
+
+
+'''a=4.5
+if type(a) is not float:
+    print("it is float")
+else:
+    print("it is not float")'''
+
+'''a="nikhita"
+if type(a) is str:
+    print("it is str")
+else:
+    print("it is not str")'''
+
+
+'''a="nikhita"
+if type(a) is not str:
+    print("it is str")
+else:
+    print("it is not str")'''
+
+
+'''a=4+5j
+if type(a) is complex:
+    print("it is complex")
+else:
+    print("it is not complex")'''
+
+'''a=4+5j
+if type(a) is not complex:
+    print("it is complex")
+else:
+    print("it is not complex")'''
+
+
+'''a=True
+if type(a) is bool:
+    print("it is bool")
+else:
+    print("it is not bool")'''
+
+
+'''a=True
+if type(a) is not bool:
+    print("it is bool")
+else:
+    print("it is not bool")'''
+
+#if-else conditions by using membership operators
+#in,not in
+'''a=2,3,4,5,6,7,8,9,10
+if 8 in a:
+    print("true")
+else:
+     print("false")'''
+
+
+'''a=2,3,4,5,6,7,8,9,10
+if 20 not in a:
+    print("true")
+else:
+     print("false")'''
+
+
+'''a=int(input("a value"))
+if 30 in a:
+    print("true")#error#runtime'''
+
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input("value"))
+if b in a:
+     print("true")
+else:
+     print("false")'''
+
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input("value"))
+if b not in a:
+    print("true")
+else:
+     print("false")'''
+
+
+
+
+
+
+
+
+
+
+
+
