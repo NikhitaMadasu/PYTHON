0de87f61b45580897eea85aa190fa07aa33560b3
NikhitaMadasu <sainikhitamadasu@gmail.com>
2026-07-07 06:41:33 +0530

    Add files via upload

---
+++ runtime.py
@@ -0,0 +1,121 @@
+#print(3+5)
+'''a=5
+b=6
+print(a+b)'''
+'''a=8
+b=9
+c=a+b
+print(c)'''
+#run_time input()
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+'''a=complex(input("data1"))
+b=complex(input("data2"))
+print(a+b)'''
+
+'''a=bool(input("data1"))
+b=bool(input("data2"))
+c=bool(input("data3"))
+print(a+b+c)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(choose 1.add 2.sub 3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=(input(choose add sub mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(choose
+                  1.add
+                  2.sub
+                  3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=input()
+print(a)'''
+
+#swapping of two numbers
+#using temp value
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print(a)
+print(b)'''
+#without using temp
+'''a=10
+b=20
+a,b=b,a
+print(a)
+print(b)'''
+#using arithmetic operator
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)'''
+#using integer
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d"%(a,b))'''
+#using float
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%f.b=%f" %(a,b))'''
+#using  string
+'''a="nikhita"
+b="madasu"
+c=a
+a=b
+b=c
+print("after swapping a=%s,b=%s" %(a,b))'''
+
+'''a="nikhita"
+b="madasu"
+a,b=b,a
+print(a)
+print(b)'''
+
+'''a=input()
+b=input()
+a,b=b,a
+print("after swapping a=%s,b=%s" %(a,b))'''
+
+
+
+
+
