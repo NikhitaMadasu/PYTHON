e251309aaf44e062e9dbddad8804cf82f8696112
NikhitaMadasu <sainikhitamadasu@gmail.com>
2026-07-07 06:35:40 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,35 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #data types
+>>> a=10
+>>> type(a)
+<class 'int'>
+>>> b=4.5
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d=7j
+>>> type(d)
+<class 'complex'>
+>>> e=True
+>>> type(e)
+<class 'bool'>
+>>> d=3+4j
+>>> type(d)
+<class 'complex'>
+>>> c="code"
+>>> type(c)
+<class 'str'>
+>>> c='''nikhita'''
+>>> type(c)
+<class 'str'>
+>>> d=4+5j
+>>> type(d)
+<class 'complex'>
+>>> e=False
+>>> type(e)
+<class 'bool'>
+>>> int(7)
+7
