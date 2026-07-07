f3b0354d4081749919acbc5197b69acb5f15d165
NikhitaMadasu <sainikhitamadasu@gmail.com>
2026-07-07 06:37:32 +0530

    Add files via upload

---
+++ Bitwise operator.py
@@ -0,0 +1,60 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=2
+>>> b=5
+>>> a&b
+0
+>>> a=6
+>>> b=9
+>>> a&b
+0
+>>> a=5
+>>> b=7
+>>> a&b
+5
+>>> bin(2)
+'0b10'
+>>> bin(7)
+'0b111'
+>>> bin(9)
+'0b1001'
+>>> a=2
+>>> b=5
+>>> a|b
+7
+>>> a=5
+>>> -(a+1)
+-6
+>>> b=-9
+>>> -(b+1)
+8
+>>> ~a
+-6
+>>> ~b
+8
+>>> c=-22
+>>> ~c
+21
+>>> a=3 b=5
+SyntaxError: invalid syntax
+>>> a=3
+>>> b=5
+>>> a^b
+6
+>>> a=3
+>>> b=6
+>>> a<<8
+768
+a<<b
+192
+a<<9
+1536
+c=8
+c<<7
+1024
+d=5
+d>>2
+1
+e=22
+22>>4
+1
