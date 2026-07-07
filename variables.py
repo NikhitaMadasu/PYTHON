67aef200411792a4f7cce7c0b6a6be8ddc43138e
NikhitaMadasu <sainikhitamadasu@gmail.com>
2026-07-07 06:34:33 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,100 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(3+3)
+6
+a=10
+print(a)
+10
+b=2
+c=4
+print(b+c)
+6
+
+print(x)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+x=20
+print(x)
+20
+z=40
+print(z)
+40
+a3=40
+print(a3)
+40
+4a=50
+SyntaxError: invalid decimal literal
+print(4a)
+SyntaxError: invalid decimal literal
+a123456789=50
+print(a123456789)
+50
+name="Nikhita"
+print(name)
+Nikhita
+city="vja"
+print(city)
+vja
+country="india"
+print(country)
+india
+india
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    india
+NameError: name 'india' is not defined
+@a=40
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+_a=40
+print(_a)
+40
+mailid("sainikhitamadasu@gmail.com")
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    mailid("sainikhitamadasu@gmail.com")
+NameError: name 'mailid' is not defined
+>>> mailid="sainikhitamadasu@gmail.com"
+>>> print(mailid)
+sainikhitamadasu@gmail.com
+>>> if=30
+SyntaxError: invalid syntax
+>>> elif=70
+SyntaxError: invalid syntax
+>>> print=10
+>>> print(print)
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    print(print)
+TypeError: 'int' object is not callable
+>>> a=4;b=9
+>>> print(a+b)
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    print(a+b)
+TypeError: 'int' object is not callable
+>>> a,b=2,3
+>>> print(a+b)
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    print(a+b)
+TypeError: 'int' object is not callable
+>>> a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
+>>> a=b=c=10
+>>> print(a,b,c)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(a,b,c)
+TypeError: 'int' object is not callable
+>>> NAME="Nikhita"
+>>> print(NAME)
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    print(NAME)
+TypeError: 'int' object is not callable
